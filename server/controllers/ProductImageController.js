e56eed8411d64678b99b0470ac71a74dda187d24
khoirulkahf <fahrulista@gmail.com>
2022-02-18 10:51:05 +0700

    Menmbahkan multer

--- server/controllers/ProductImageController.js
+++ server/controllers/ProductImageController.js
@@ -2,24 +2,45 @@ const { products_image } = require("../models");
 
 class ProductImageController {
   static async add(req, res) {
-        try {
-            const productId = +req.params.id
-            let prim_file_name =  req.file.filename
-            let prim_file_size =  req.file.size
-            let prim_file_type =  req.file.mimetype
-            
-                let result = await products_image.create({
-                    prim_file_name,
-                    prim_file_size,
-                    prim_file_type,
-                    prim_primary:true,
-                    productId
-                })
-                res.status(201).json(result);
-            
-        } catch (e) {
-            res.status(500).json(e)
+    try {
+      const productId = +req.params.id;
+      let prim_file_name = req.file.filename;
+      let prim_file_size = req.file.size;
+      let prim_file_type = req.file.mimetype;
+
+      let result = await products_image.create({
+        prim_file_name,
+        prim_file_size,
+        prim_file_type,
+        prim_primary: true,
+        productId,
+      });
+      res.status(201).json(result);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+  static async update(req, res) {
+    try {
+      const productId = +req.params.id;
+      let prim_file_name = req.file.filename;
+      let prim_file_size = req.file.size;
+      let prim_file_type = req.file.mimetype;
+
+      let result = await products_image.update(
+        {
+          prim_file_name,
+          prim_file_size,
+          prim_file_type,
+        },
+        {
+          where: { productId },
         }
+      );
+      res.status(201).json(result);
+    } catch (e) {
+      res.status(500).json(e);
+    }
   }
 }
 
--- server/routes/productsImage.js
+++ server/routes/productsImage.js
@@ -7,7 +7,7 @@ const { store } = require('../middlewares/multer')
 // productImageRoute.get('/', authentication, ProductImageController.getProducts)
 // productImageRoute.get('/:id', authentication, ProductImageController.getById)
 productImageRoute.post('/add/:id', store, ProductImageController.add)
-// productImageRoute.put('/edit/:id', authentication, authorization, ProductImageController.update)
+productImageRoute.put('/edit/:id', authentication, authorization, ProductImageController.update)
 // productImageRoute.delete('/delete/:id', authentication, authorization, ProductImageController.remove)
 
 module.exports = productImageRoute
